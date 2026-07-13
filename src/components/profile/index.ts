88aa32888b9f023ebd01b36bc24c5cb98146c5a8
Ajit Kumar <39993392+anmolanubhab@users.noreply.github.com>
2026-07-13 16:38:00 +0530

    Export multiple components from profile index

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,1 +1,7 @@
-
+export { default as ProfileHeader } from "./ProfileHeader";
+export { default as PersonalInfoCard } from "./PersonalInfoCard";
+export { default as CompanyCard } from "./CompanyCard";
+export { default as SecurityCard } from "./SecurityCard";
+export { default as PermissionCard } from "./PermissionCard";
+export { default as PreferenceCard } from "./PreferenceCard";
+export { default as ActivityCard } from "./ActivityCard";
